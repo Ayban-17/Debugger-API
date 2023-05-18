bec12ebc90ddd0f699701e95c014051e44952063
Ayban <mrkvnhrnndz17@gmail.com>
2023-05-18 22:11:09 +0800

    last commit

---
+++ controllers/gptResponseController.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+dotenv.config();
+
+const url = process.env.GPT_URI;
+const key = process.env.API_KEY;
+let answer;
+
+const fetchData = async (url, language, code) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Please analyze my ${language} code and identify any issues or errors. Additionally, provide the corrected version of the code with comments to help beginners understand it. Here is the code I would like you to review: 
+
+          ${code}
+
+          Please provide clarity for beginner programmers. Thank you!`,
+        },
+      ],
+      temperature: 0.2,
+    }),
+  });
+  const data = await response.json();
+  const message = await data.choices[0].message.content;
+  answer = message;
+};
+
+const getAnswer = async (req, res) => {
+  const { language, code } = req.body;
+  try {
+    await fetchData(url, language, code);
+    res.json({ msg: answer });
+  } catch (error) {
+    console.log(`The error is ${error}`);
+  }
+};
+
+export default getAnswer;
---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import gptRouter from "./routes/gptResponse.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+import dotenv from "dotenv";
+
+dotenv.config();
+app.use("/api/v1/openAI", gptRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
